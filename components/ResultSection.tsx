f8f1650543860aa0916b68796d78b10088ab8096
Oka Wibawa <okaa.wibawa@gmail.com>
2021-12-18 00:49:48 +0800

    adds sort function

--- components/ResultSection.tsx
+++ components/ResultSection.tsx
@@ -1,9 +1,31 @@
 import { useState } from 'react';
 import Image from 'next/image';
+import { DateTime } from 'luxon';
 import styles from '../styles/ResultSection.module.css';
 
 const ResultSection = ({ data, repos }: any) => {
   const [pagination, setPagination] = useState(4);
+  const [sort, setSort] = useState();
+
+  let newRepos;
+
+  const handleChangeSort = (e: any) => {
+    setSort(e.target.value);
+  };
+
+  if (sort) {
+    if (sort === 'oldest') {
+      newRepos = repos.sort((a: any, b: any) => new Date(a.created_at).valueOf() - new Date(b.created_at).valueOf());
+    } else if (sort === 'newest') {
+      newRepos = repos.sort((a: any, b: any) => new Date(b.created_at).valueOf() - new Date(a.created_at).valueOf());
+    } else if (sort === 'default') {
+      newRepos = repos.sort((a: any, b: any) => a.name.localeCompare(b.name));
+    }
+  } else {
+    newRepos = repos;
+  }
+
+  console.log(newRepos);
 
   return (
     <section className={styles.section__container}>
@@ -33,14 +55,36 @@ const ResultSection = ({ data, repos }: any) => {
       <div style={{ height: '1px', width: '100%', backgroundColor: '#444C56', margin: '1rem' }}></div>
 
       <div className={styles.section__profile__repos}>
-        {repos.length > 0 ? (
+        {newRepos.length > 0 ? (
           <div>
             <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'flex-end' }}>
-              {/* <p style={{ fontSize: '14px', color: '#c4c4c4', fontWeight: 'bold' }}>Sort:</p> */}
+              <p style={{ fontSize: '14px', color: '#c4c4c4', fontWeight: 'bold' }}>Sort: </p>
+              <select
+                name="date"
+                id="date"
+                form="datesort"
+                style={{
+                  marginLeft: '8px',
+                  backgroundColor: '#3D464A',
+                  boxShadow: 'none',
+                  borderRadius: '6px',
+                  border: '1px solid #444C56',
+                  padding: '10px 12px',
+                  fontSize: '14px',
+                  color: 'white',
+                  appearance: 'none',
+                  fontWeight: 'bold',
+                }}
+                onChange={handleChangeSort}
+              >
+                <option value="default">Default</option>
+                <option value="newest">Newest</option>
+                <option value="oldest">Oldest</option>
+              </select>
             </div>
 
             <div className={styles.repos__container}>
-              {repos.slice(0, pagination).map((repo: any) => (
+              {newRepos.slice(0, pagination).map((repo: any) => (
                 <div
                   style={{
                     backgroundColor: '#3D464A',
@@ -63,15 +107,33 @@ const ResultSection = ({ data, repos }: any) => {
                     {repo.description ? repo.description : '-'}
                   </p>
 
-                  <div style={{ display: 'flex', alignItems: 'center', position: 'absolute', left: 16, bottom: 16 }}>
+                  <div
+                    style={{
+                      display: 'flex',
+                      alignItems: 'center',
+                      justifyContent: 'space-between',
+                      position: 'absolute',
+                      right: 16,
+                      left: 16,
+                      bottom: 16,
+                    }}
+                  >
                     <div style={{ display: 'flex', alignItems: 'center' }}>
-                      <Image src="/star.svg" height="16" width="16" alt="star" />
-                      <p style={{ color: '#c4c4c4', fontSize: '12px', marginLeft: '4px' }}>{repo.stargazers_count}</p>
+                      <div style={{ display: 'flex', alignItems: 'center' }}>
+                        <Image src="/star.svg" height="16" width="16" alt="star" />
+                        <p style={{ color: '#c4c4c4', fontSize: '12px', marginLeft: '4px' }}>{repo.stargazers_count}</p>
+                      </div>
+
+                      <div style={{ display: 'flex', alignItems: 'center', marginLeft: '12px' }}>
+                        <Image src="/git-branch.svg" height="16" width="16" alt="star" />
+                        <p style={{ color: '#c4c4c4', fontSize: '12px', marginLeft: '4px' }}>{repo.forks_count}</p>
+                      </div>
                     </div>
 
-                    <div style={{ display: 'flex', alignItems: 'center', marginLeft: '12px' }}>
-                      <Image src="/git-branch.svg" height="16" width="16" alt="star" />
-                      <p style={{ color: '#c4c4c4', fontSize: '12px', marginLeft: '4px' }}>{repo.forks_count}</p>
+                    <div>
+                      <p style={{ color: '#c4c4c4', fontSize: '12px' }}>
+                        {DateTime.fromISO(repo.created_at).toLocaleString()}
+                      </p>
                     </div>
                   </div>
                 </div>
